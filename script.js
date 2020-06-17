dae2f9a25ec11c3a3db01ff0ffe8935ba1f35843
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2020-06-17 20:08:23 +0900

    프로필 추가시 유효성 검사 처리

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ function getCarrierName(carrierCode) {
     return name;
 }
 
+function getWay(wayCode) {
+    if(wayCode == '1') {
+        var str = 'SMS';
+    }else{
+        var str = "PASS";
+    }
+    return str;
+}
+
 
 function addProfile(prof) {
     chrome.storage.sync.get(function (data) {
@@ -74,22 +83,61 @@ window.onload = function () {
     }, false);
 
     document.getElementById('addProfile').addEventListener('click', function () {
-        var name = document.getElementById('name').value;
-        var carrier = document.getElementById('carrier').value;
-        var phone_number = document.getElementById('phone_number').value;
-        var birth = document.getElementById('birth').value;
-        var gender = document.getElementById('gender').value;
-        var foreigner = document.getElementById('foreigner').value;
-        var way = document.getElementById('way').value;
+        var name = document.getElementById('name');
+        var carrier = document.getElementById('carrier');
+        var phone_number = document.getElementById('phone_number');
+        var birth = document.getElementById('birth');
+        var gender = document.getElementById('gender');
+        var foreigner = document.getElementById('foreigner');
+        var way = document.getElementById('way');
+
+        if (name.value.length < 2) {
+            window.alert("이름을 입력하시오");
+            name.focus();
+            name.select();
+            return false;
+        }
+        
+        if (carrier.value == '-1') {
+            window.alert("통신사를 선택하시오");
+            carrier.focus();
+            return false;
+        }
+
+        if (phone_number.value.length < 10 || phone_number.value.length > 11) {
+            window.alert("휴대폰번호를 확인하시오");
+            phone_number.focus();
+            phone_number.select();
+            return false;
+        }
+
+        if (birth.value.length != 8) {
+            window.alert("생일을 확인하시오 (8자리) (예, 19990204)");
+            birth.focus();
+            birth.select();
+            return false;
+        }
+
+        if (gender.value == '-1') {
+            window.alert("성별을 선택하시오");
+            gender.focus();
+            return false;
+        }
+
+        if (way.value == '-1') {
+            window.alert("인증방식을 선택하시오");
+            way.focus();
+            return false;
+        }
 
         profile = {
-            "name": name,
-            "carrier": carrier,
-            "phone_number": phone_number,
-            "birth": birth,
-            "gender": gender,
-            "foreigner": foreigner,
-            "way": way
+            "name": name.value,
+            "carrier": carrier.value,
+            "phone_number": phone_number.value,
+            "birth": birth.value,
+            "gender": gender.value,
+            "foreigner": foreigner.value,
+            "way": way.value
         };
 
         addProfile(profile);
@@ -117,6 +165,7 @@ window.onload = function () {
                                             <div class="widget-heading">' + profilesOb.profiles[i].name + '(' + profilesOb.profiles[i].phone_number + ')</div>\
                                             <div class="widget-subheading">\
                                                 <div class="badge badge-pill badge-info">' + getCarrierName(profilesOb.profiles[i].carrier) + '</div>\
+                                                <div class="badge badge-pill badge-warning">' + getWay(profilesOb.profiles[i].way) + '</div>\
                                             </div>\
                                         </div>\
                                         <div class="widget-content-right">' + (selectedProfile != i ? '  <button class="SelectBtn border-0 btn-transition btn btn-outline-success">\
@@ -126,9 +175,6 @@ window.onload = function () {
                                 </div>';
                 ul.appendChild(li);
             }
-
         }
     });
-
-
 };
