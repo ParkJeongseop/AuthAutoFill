3058869e2b37ec76f93bc5b4bcfa222b4a21be39
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2023-04-24 13:24:08 +0900

    개발: 교통민원24 자동완성 추가

--- autofill.js
+++ autofill.js
@@ -563,6 +563,55 @@ window.onload = function () {
                         }
                     }, 500);
                     
+                } else if (window.location.hostname == 'www.efine.go.kr') {
+                    this.console.log('교통민원24');
+                    
+                    setInterval(function() {
+                        var nameInput = this.document.querySelector("#oacx_name");
+                        var birthDateInput = this.document.querySelector("#oacx_num1");
+                        var rrnInput = this.document.querySelector("#oacx_num2")
+                        var phone1Input = this.document.querySelector("#submitFm > table > tbody > tr.telecom > td > select:nth-child(2)");
+                        var phone1Input2 = document.querySelector("#oacx_phone1")
+                        var phone2Input = this.document.querySelector("#oacx_phone3");
+                        var phone2Input2 = this.document.querySelector("#oacx_phone2");
+                        var carrierInput = this.document.querySelector("#submitFm > table > tbody > tr.telecom > td > select:nth-child(1)");
+                        var agreeInput = this.document.querySelector("#oacx_total");
+
+                        if(nameInput) {
+                            nameInput.value = profilesOb[spI].name;
+                        }
+                        if(birthDateInput) {
+                            birthDateInput.value = profilesOb[spI].birth.substr(2, 6);
+                        }
+                        if(phone1Input) {
+                            phone1Input.value = profilesOb[spI].phone_number.substr(0, 3);
+                        }
+                        if(phone1Input2) {
+                            phone1Input2.value = profilesOb[spI].phone_number.substr(0, 3);
+                        }
+                        if(phone2Input) {
+                            phone2Input.value = profilesOb[spI].phone_number.substr(3, 8);
+                        }
+                        if(phone2Input2) {
+                            phone2Input2.value = profilesOb[spI].phone_number.substr(3, 8);
+                        }
+                        if(carrierInput) {
+                            if (profilesOb[spI].carrier == carrier.SKT || profilesOb[spI].carrier == carrier.SKT_MVNO) {
+                                carrierInput.value = 'S';
+                            } else if (profilesOb[spI].carrier == carrier.KT || profilesOb[spI].carrier == carrier.KT_MVNO) {
+                                carrierInput.value = 'K';
+                            } else if (profilesOb[spI].carrier == carrier.LGU || profilesOb[spI].carrier == carrier.LGU_MVNO) {
+                                carrierInput.value = 'L';
+                            }
+                        }
+                        if(agreeInput && !agreeInput.checked) {
+                            agreeInput.click();
+                            if(rrnInput) {
+                                rrnInput.focus();
+                            }
+                        }
+                    }, 500);
+                    
                 }
             }
         } else {
