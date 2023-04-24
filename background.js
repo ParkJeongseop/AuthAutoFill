af48a561781f93526aeaed047fd5ca4352c595b2
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2023-04-24 15:03:04 +0900

    삭제: legacy file

--- background.js
+++
@@ -1,5 +0,0 @@
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.from == 'content_script') {
-//         console.log('The request has been received from the content script.');
-//     }
-// });
