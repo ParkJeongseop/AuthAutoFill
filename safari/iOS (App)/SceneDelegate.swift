bcd26ace7b92d756ea070bbb33c71a8aadc00206
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2024-07-15 05:25:48 +0900

    2.0.7 버전 safari macOS, ios

---
+++ safari/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by 박정섭 on 2024. 7. 15..
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
