18c9f32a584cbd4d83495dc5a6f2ec9716b315ac
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2024-07-16 18:39:03 +0900

    mac에서 버튼 클릭 오류 수정 (bundle Identifier)

--- safari/Shared (App)/ViewController.swift
+++ safari/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.parkjeongseop.Auth-Autofill.Extension"
+let extensionBundleIdentifier = "com.parkjeongseop.AuthAutofill.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
